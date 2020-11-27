3ff66e3c190f7f41fa14389cb1eb4d74c7f36c49
Helen Zhou <helen.zhou1@nhs.net>
2020-11-27 11:58:26 +0000

    PRMT-1408 [HZ/DS] Adds Technical Error Counts for Feb, Sept and Oct 2020

--- scripts/gp2gp_spine_outcomes.py
+++ scripts/gp2gp_spine_outcomes.py
@@ -1,22 +1,11 @@
-from gp2gp.service.models import ERROR_SUPPRESSED
+from gp2gp.service.models import ERROR_SUPPRESSED, Transfer
 from gp2gp.service.transformers import EIGHT_DAYS_IN_SECONDS, derive_transfers
-from gp2gp.date.range import DateTimeRange
 from gp2gp.pipeline.dashboard.main import read_spine_csv_gz_files
 from gp2gp.spine.transformers import parse_conversation, group_into_conversations, ConversationMissingStart
 
 from collections import defaultdict
-from datetime import datetime
-from dateutil.tz import tzutc
 
-august_data_file_name="spine_aug.csv.gz"
-september_data_file_name = "spine_sep.csv.gz"
-
-august = DateTimeRange(
-    datetime(year=2020, month=8, day=1, tzinfo=tzutc()),
-    datetime(year=2020, month=9, day=1, tzinfo=tzutc()),
-)
-
-def parse_conversations(messages, time_range):
+def _parse_conversations(messages, time_range):
     for conversation in group_into_conversations(messages):
         try:
             gp2gp_conversation = parse_conversation(conversation)
@@ -25,11 +14,15 @@ def parse_conversations(messages, time_range):
         except ConversationMissingStart:
             pass
 
-def outcome(transfer):
-    if transfer.pending: # "is not completed"
-        return "DIDN'T COMPLETE - POSSIBLE ERROR OR STILL WAITING"
+def outcome(transfer: Transfer):
+    if transfer.pending:
+        if len(transfer.intermediate_error_codes) > 0:
+            return "DIDN'T COMPLETE - ERROR MID CONVERSATION"
+        else:
+            return "DIDN'T COMPLETE - STUCK"
+          
     else:
-        if transfer.error_code is None or transfer.error_code == ERROR_SUPPRESSED: # "final error code"
+        if transfer.final_error_code is None or transfer.final_error_code == ERROR_SUPPRESSED: # "final error code"
             if transfer.sla_duration is None:
                 raise Exception(f"Completed transfer had no SLA! {transfer.conversation_id}")
             elif transfer.sla_duration.total_seconds() <= EIGHT_DAYS_IN_SECONDS:
@@ -37,18 +30,20 @@ def outcome(transfer):
             else:
                 return "COMPLETED - BEYOND 8 DAYS"
         else:
-            return "COMPLETED - ERROR"
+            return "COMPLETED - ERROR IN FINAL ACK"
 
-spine_messages = read_spine_csv_gz_files([
-    august_data_file_name, september_data_file_name
-])
 
-conversations = parse_conversations(spine_messages, time_range=august)
-transfers = derive_transfers(conversations)
+def calculate_counts(month_file_name: str, next_month_file_name: str, time_range):
+  spine_messages = read_spine_csv_gz_files([
+      month_file_name, next_month_file_name
+  ])
 
-counts = defaultdict(int)
-for gp2g_transfer in transfers:
-    counts[outcome(gp2g_transfer)] += 1
+  conversations = _parse_conversations(spine_messages, time_range=time_range)
+  transfers = derive_transfers(conversations)
 
-print(counts)
+  counts = defaultdict(int)
+  for gp2g_transfer in transfers:
+      counts[outcome(gp2g_transfer)] += 1
+
+  print(counts)
 

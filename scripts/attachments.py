790cc2676f71c033a3ecf8bbe0faacbc20e3958a
James Green <jagreen@thoughtworks.com>
2021-01-11 10:57:34 +0000

    PRMT-1181 [HZ/JG] Produce high level stats of attachment sizes

--- scripts/attachments.py
+++ scripts/attachments.py
@@ -28,7 +28,8 @@ CREATE_OPTIONAL_INT_MACRO_STATEMENT = """
     CASE WHEN field='Unknown' THEN NULL ELSE CAST(field AS INT) END
 """
 
-#TODO: Rewrite using relation api?
+
+# TODO: Rewrite using relation api?
 def load_attachment_metadata_statement(filename):
     return f"""
         INSERT INTO attachment_metadata
@@ -47,28 +48,59 @@ def load_attachment_metadata_statement(filename):
         FROM read_csv_auto('{filename}', all_varchar=TRUE);
     """
 
-def construct_db_database(input_data_dir, database_file):
+
+def load_attachment_metadata(cursor, input_data_dir):
     input_data_dir_path = Path(input_data_dir)
     attachments_data_files = input_data_dir_path.rglob("*csv")
-
-    cursor = duckdb.connect(database_file)
 
     cursor.execute(CREATE_YES_NO_MACRO_STATEMENT)
     cursor.execute(CREATE_OPTIONAL_INT_MACRO_STATEMENT)
     cursor.execute(CREATE_ATTACHMENT_METADATA_TABLE_STATEMENT)
 
     for data_file in attachments_data_files:
-        print(f"Loading {data_file}")
         cursor.execute(load_attachment_metadata_statement(data_file))
 
-    print("Done")
-    cursor.close()
+    return cursor.table("attachment_metadata")
+
+
+def create_view_attachments_count_per_conversation(attachment_metadata):
+    return attachment_metadata \
+        .aggregate("conversation_id, count(*) AS number_attachments") \
+        .order("number_attachments desc") \
+        .create_view("attachments_per_conversation")
+
+
+def create_view_attachment_count_frequency(attachments_per_conversation):
+    return attachments_per_conversation \
+        .aggregate("number_attachments, count(*) AS transfer_count") \
+        .order("number_attachments desc") \
+        .create_view("number_attachment_frequency")
+
+
+def create_view_attachment_statistics(attachment_metadata):
+    return attachment_metadata \
+        .aggregate("count(*) AS total,"
+                   "MIN(length) AS smallest,"
+                   "QUANTILE(length, 0.25) AS quantile_25,"
+                   "MEDIAN(length) AS median,"
+                   "QUANTILE(length, 0.75) AS quantile_75,"
+                   "MAX(length) AS largest") \
+        .create_view("attachment_statistics")
+
+
+def construct_attachments_db(cursor, input_data_dir):
+    attachment_metadata = load_attachment_metadata(cursor, input_data_dir)
+    create_view_attachment_statistics(attachment_metadata)
+    attachment_count_per_conversation = create_view_attachments_count_per_conversation(attachment_metadata)
+    create_view_attachment_count_frequency(attachment_count_per_conversation)
 
 
 def main():
     input_data_dir = argv[1]
     database_file = argv[2]
-    construct_db_database(input_data_dir, database_file)
+    cursor = duckdb.connect(database_file)
+    construct_attachments_db(cursor, input_data_dir)
+    cursor.close()
 
 
 if __name__ == "__main__":
